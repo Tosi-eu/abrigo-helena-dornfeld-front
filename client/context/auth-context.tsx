4aa4eaa357a5084da2a7599fc35513ee3d008950
Tosi-eu <galdiniguilherme@usp.br>
2025-11-10 10:49:14 -0300

    fix: prevent user from being logged out on page refresh

--- client/context/auth-context.tsx
+++ client/context/auth-context.tsx
@@ -1,16 +1,16 @@
+import { createContext, useState, useEffect, ReactNode } from "react";
 import { AuthContextType, LoggedUser } from "@/interfaces/interfaces";
-import { createContext, useState, ReactNode, useEffect } from "react";
 
-export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined,
-);
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<LoggedUser | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) setUser(JSON.parse(storedUser));
+    setLoading(false);
   }, []);
 
   const login = async (login: string, password: string) => {
@@ -30,6 +30,8 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     setUser(null);
     localStorage.removeItem("user");
   };
+
+  if (loading) return null; 
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
